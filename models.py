ff4267b2af5b2410839aa135d0efea520fb116ee
UserBlackBox <userblackbox@tutanota.com>
2021-01-01 15:59:01 -0500

    database stuff

--- models.py
+++ models.py
@@ -2,6 +2,12 @@ from . import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
+    theme = db.Column(db.String(1000))
+
+class Entry(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user = db.Column(db.Integer, unique=True)
+    date = db.Column(db.DateTime)
+    entry = db.column(db.String(10000))
