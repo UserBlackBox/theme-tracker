2cfcebcc2cc194ca08a3fca126adc5d7572b9af1
UserBlackBox <userblackbox@tutanota.com>
2021-01-02 14:01:17 -0500

    suggested activities

--- main.py
+++ main.py
@@ -16,7 +16,20 @@ def index():
     new = new[:5]
     recent = Entry.query.filter_by(user=current_user.name).order_by(Entry.id).all()[::-1]
     recent = recent[:5]
+    tags = set()
+    tags.add(current_user.theme.lower())
+    entries = Entry.query.filter_by(user=current_user.name).order_by(Entry.id).all()[::-1]
+    for i in entries:
+        for j in i.tags.split():
+            tags.add(j)
+    entries = Entry.query.filter(Entry.user!=current_user.name).order_by(Entry.id).all()[::-1]
     suggested = []
+    for i in entries:
+        for j in i.tags.split():
+            if j in tags:
+                suggested.append(i)
+                break
+    suggested = suggested[:5]
 
     return render_template('home.html', new=new, recent=recent, suggested=suggested)
 
@@ -38,4 +51,17 @@ def your():
 @main.route('/suggested/')
 @login_required
 def suggested():
-    return render_template('activities.html')
+    tags = set()
+    tags.add(current_user.theme.lower())
+    entries = Entry.query.filter_by(user=current_user.name).order_by(Entry.id).all()[::-1]
+    for i in entries:
+        for j in i.tags.split():
+            tags.add(j)
+    entries = Entry.query.filter(Entry.user!=current_user.name).order_by(Entry.id).all()[::-1]
+    suggested = []
+    for i in entries:
+        for j in i.tags.split():
+            if j in tags:
+                suggested.append(i)
+                break
+    return render_template('activities.html', suggested = suggested)
