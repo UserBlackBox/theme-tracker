7c9466bee137e0f4ac04535ebec6ac8897592476
UserBlackBox <userblackbox@tutanota.com>
2021-01-02 01:04:13 -0500

    add key

--- __init__.py
+++ __init__.py
@@ -6,6 +6,7 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'T0ZFQyA-NpBzLWS_fliFkg'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
     db.init_app(app)
     from .auth import auth as auth_blueprint
