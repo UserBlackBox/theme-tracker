b49a0ef3966eb0e051988a94da2c67553d58aa4f
UserBlackBox <userblackbox@tutanota.com>
2021-01-02 15:05:20 -0500

    create new entries!

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, request
 from flask_login import login_required, current_user
 from . import db
 from .models import Entry
@@ -65,3 +65,24 @@ def suggested():
                 suggested.append(i)
                 break
     return render_template('activities.html', suggested = suggested)
+
+@main.route('/new/', methods=['GET'])
+@login_required
+def new_entry():
+    return render_template('new-entry.html')
+
+@main.route('/new/', methods=['POST'])
+@login_required
+def new_entry_post():
+    entry = request.form.get('entry')
+    tags = request.form.get('tags').lower()
+    date = datetime.datetime.strptime(request.form.get('date'), '%Y-%m-%d')
+    entries = Entry.query.order_by(Entry.id).all()
+    if len(entries) == 0:
+        id = 1
+    else:
+        id = entries[-1].id+1
+    new_entry = Entry(id=id, user=current_user.name, date=date, entry=entry, tags=tags)
+    db.session.add(new_entry)
+    db.session.commit()
+    return redirect(url_for('main.your'))
